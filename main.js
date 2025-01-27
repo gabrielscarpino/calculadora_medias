92eff1e4033fdef0ab6aaa3f92c0492df8574243
gabriel <gabrielscarpino@hotmail.com>
2025-01-27 18:11:43 -0300

    fix: limitação de casas decimais

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ function atualizaMediaFinal (){
 
     const MediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = MediaFinal;
+    document.getElementById('media-final-valor').innerHTML = MediaFinal.toFixed(2);
     document.getElementById('media-final-resultado').innerHTML = MediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
